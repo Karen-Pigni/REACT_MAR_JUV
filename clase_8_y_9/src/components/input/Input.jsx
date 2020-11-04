33f1246d9d128e4eec327759cd76698565b18006
facu diaz <facu.diaz777@gmail.com>
2020-11-03 21:24:21 -0300

    final clase 9 custom hook useForm

--- clase_8_y_9/src/components/input/Input.jsx
+++ clase_8_y_9/src/components/input/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { validate } from '../../utils/validators';
 import './input.css';
 
@@ -30,6 +30,13 @@ const Input = (props) => {
     isTouche: false,
     isValid: props.initialValid || false,
   });
+
+  const { isValid, value } = inputState;
+  const { onInput, id } = props;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [onInput, id, value, isValid]);
 
   const changeHandler = (event) => {
     dispatch({
